4d874a9c3e2e4791ed630a56197afea025bb5d28
ezu0 <burimofumofu@gmail.com>
2024-11-06 17:23:54 +0900

    add: エロクワント、クエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
@@ -11,6 +12,20 @@ class TestController extends Controller
     public function index()
     {
         $values = Test::all();
+        $count = Test::count();
+        $first = Test::findOrFail(1);
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+
+        //エロクワント vs クエリビルダ
+        //どっちもセキュリティ面で優れている
+        //エロクワントの方がDBの連結やクエリの分割などにおいて便利
+
+        //クエリビルダ 基本getでとる
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+            ->select('id', 'text')
+            ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
 
         //dd($values);
         return view('tests.test', compact('values'));

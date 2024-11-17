1f384c0341eef2fac40d64fb16aec6ef572a5619
ezu0 <burimofumofu@gmail.com>
2024-11-17 14:29:46 +0900

    add: 更新画面

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -91,7 +91,8 @@ class ContactFormController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        return view('contacts.edit', compact('contact'));
     }
 
     /**
